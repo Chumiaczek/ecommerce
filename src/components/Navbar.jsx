3299c8dc39d161c1a41d6bee04a39e73afc000f7
Grzegorz Tabat <prywatne.grzegorztabat@gmail.com>
2022-04-30 21:42:51 +0200

    1

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,68 +1,66 @@
-import { Badge } from '@material-ui/core'
-import { Search, ShoppingCartOutlined } from '@material-ui/icons'
-import React from 'react'
-import styled from 'styled-components'
+import { Badge } from "@material-ui/core";
+import { Search, ShoppingCartOutlined } from "@material-ui/icons";
+import React from "react";
+import styled from "styled-components";
 
 const Container = styled.div`
   height: 60px;
-`
+`;
 const Wrapper = styled.div`
   padding: 6px 20px;
   display: flex;
   justify-content: space-between;
   align-items: center;
-`
+`;
 const Left = styled.div`
   flex: 1;
   display: flex;
   align-itmes: center;
-`
+`;
 const Language = styled.span`
   font-size: 14;
   cursor: pointer;
-  display:flex;
+  display: flex;
   align-items: center;
-`
+`;
 const Input = styled.input`
   border: none;
-`
+`;
 const SearchContainer = styled.div`
   border: 0.5px solid lightgray;
   display: flex;
   align-items: center;
   margin-left: 25px;
   padding: 5px;
-`
+`;
 const Center = styled.div`
   flex: 1;
   text-align: center;
-`
+`;
 const Logo = styled.h1`
   font-weight: bold;
-`
+`;
 const Right = styled.div`
   flex: 1;
   display: flex;
   align-items: center;
   justify-content: flex-end;
-`
+`;
 const MenuItem = styled.div`
   font-size: 14px;
   cursor: pointer;
   margin-left: 25px;
-`
+`;
 
 const Navbar = () => {
   return (
     <Container>
       <Wrapper>
         <Left>
-          <Language>
-            EN
-          </Language>
+          <Language>EN</Language>
           <SearchContainer>
-            <Input/>
-            <Search style={{color: "gray", fontSize: 16}}/>
+            <Input />
+            <Search style={{ color: "gray", fontSize: 16 }} />
           </SearchContainer>
         </Left>
         <Center>
@@ -73,13 +71,13 @@ const Navbar = () => {
           <MenuItem>SIGN IN</MenuItem>
           <MenuItem>
             <Badge badgeContent={4} color="primary">
-              <ShoppingCartOutlined/>
+              <ShoppingCartOutlined />
             </Badge>
           </MenuItem>
         </Right>
       </Wrapper>
     </Container>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
